476a873afd16f353d87989191da8d6219d530547
Marcin Siuda <msiuda@MacBook-Air-Marcin.local>
2024-05-12 21:12:09 +0200

    init

---
+++ src/test/java/com/example/mockolingo/MockolingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockolingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockolingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
