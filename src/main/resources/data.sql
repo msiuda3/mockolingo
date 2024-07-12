0597a722e09defc7c8d0d4c6ab371f57fc891f67
msiuda <siudamarcin3@gmail.com>
2024-07-12 15:19:06 +0200

    testowe dane

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,16 @@
 -- Wstawianie użytkowników
 INSERT INTO _user (firstname, lastname, username, password, role) VALUES
-('John', 'Doe', 'john_doe', '$2a$10$mIg03NoYOT5Tg5/4EuYylO0ag91m/2zkAfHN0XoPGB1TB/Kj7rqpG', 'USER'),
-('Admin', 'User', 'admin_user', '$2a$10$mIg03NoYOT5Tg5/4EuYylO0ag91m/2zkAfHN0XoPGB1TB/Kj7rqpG', 'ADMIN'),
-('Manager', 'User', 'manager_user', '$2a$10$mIg03NoYOT5Tg5/4EuYylO0ag91m/2zkAfHN0XoPGB1TB/Kj7rqpG', 'MANAGER');
+('John', 'Doe', 'normal_user', '$2a$10$mIg03NoYOT5Tg5/4EuYylO0ag91m/2zkAfHN0XoPGB1TB/Kj7rqpG', 'USER'),
+('Admin', 'User', 'admin_user', '$2a$10$mIg03NoYOT5Tg5/4EuYylO0ag91m/2zkAfHN0XoPGB1TB/Kj7rqpG', 'ADMIN');
 
 -- Wstawianie quizu
 INSERT INTO _quiz (quiz_name) VALUES
-('Geography Quiz');
+('Quiz 1'),
+('Quiz 2');
 
 -- Wstawianie pytań i przypisywanie ich do quizu
 INSERT INTO _question (question, a, b, c, correct_answer, quiz_id) VALUES
-('What is the capital of France?', 'Berlin', 'Madrid', 'Paris', 'C', 1),
-('What is the largest ocean?', 'Atlantic', 'Indian', 'Pacific', 'C', 1);
+('Jeść', 'to eat', 'to sleep', 'to jump', 'A', 1),
+('Pić', 'to sleep', 'to eat', 'to drink', 'C', 1);
+('Pies', 'cat', 'dog', 'horse', 'B', 2);
+('Ptak', 'bird', 'cat', 'fish', 'A', 2);
